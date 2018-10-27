d295585d4e8e6f5ba9eb3a470ae3e065ba8ab1e1
Brett Raye <brettraye+github@gmail.com>
2018-10-27 08:45:25 -0400

    form_data -> json

--- src/Controller/SsoController.php
+++ src/Controller/SsoController.php
@@ -34,7 +34,7 @@ class SsoController extends AbstractController
                 'Content-Type' => 'application/json',
                 'Authorization' => 'Basic ' . base64_encode(getenv('ESI_CLIENT_ID') . ':' . getenv('ESI_SECRET_KEY'))
             ],
-            'form_params' => [
+            'json' => [
                 json_encode(
                     [
                         'grant_type' => 'authorization_code',
