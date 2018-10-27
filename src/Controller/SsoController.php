8d7502736f7e0952ba252f6a2b9b8dda88fb0522
Brett Raye <brettraye+github@gmail.com>
2018-10-27 02:00:27 -0400

    Use statements for request and guzzle

--- src/Controller/SsoController.php
+++ src/Controller/SsoController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use GuzzleHttp\Client;
 
 class SsoController extends AbstractController
 {
